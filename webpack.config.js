7e3d55059854ea0947bd51c8171e5e6607a32e85
Creativoo <creativoo@gmail.com>
2023-06-06 13:59:23 -0700

    preparando el proyecto para deploy

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,14 @@ const Dotenv = require("dotenv-webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  // mode: 'production', // LE INDICO EL MODO EXPLICITAMENTE
-  entry: "./src/index.js", // el punto de entrada de mi aplicación
+  entry: "./src/index.js",
   output: {
-    // Esta es la salida de mi bundle
     path: path.resolve(__dirname, "dist"),
-    // resolve lo que hace es darnos la ruta absoluta de el S.O hasta nuestro archivo
-    // para no tener conflictos entre Linux, Windows, etc
     filename: "[name].[contenthash].js",
-    // EL NOMBRE DEL ARCHIVO FINAL,
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
-    extensions: [".js"], // LOS ARCHIVOS QUE WEBPACK VA A LEER
+    extensions: [".js"],
     alias: {
       "@utils": path.resolve(__dirname, "src/utils/"),
       "@templates": path.resolve(__dirname, "src/templates/"),
@@ -29,11 +24,10 @@ module.exports = {
     },
   },
   module: {
-    // REGLAS PARA TRABAJAR CON WEBPACK
     rules: [
       {
-        test: /\.m?js$/, // LEE LOS ARCHIVOS CON EXTENSION .JS,
-        exclude: /node_modules/, // IGNORA LOS MODULOS DE LA CARPETA
+        test: /\.m?js$/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
@@ -43,35 +37,39 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
       },
       {
-        test: /\.png/, // REGLA PARA ACEPTAR IMAGENES .PNG
+        test: /\.png/,
         type: "asset/resource",
       },
       {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/fonts/[hash][ext]",
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[contenthash].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "../assets/fonts/",
+            esModule: false,
+          },
         },
       },
     ],
   },
-  // SECCION DE PLUGINS
   plugins: [
     new HtmlWebpackPlugin({
-      // CONFIGURACIÓN DEL PLUGIN
-      inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
-      template: "./public/index.html", // LA RUTA AL TEMPLATE HTML
-      filename: "./index.html", // NOMBRE FINAL DEL ARCHIVO
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
     }),
     new MiniCssExtractPlugin({
       filename: "assets/[name].[contenthash].css",
-    }), // INSTANCIAMOS EL PLUGIN
+    }),
     new CopyPlugin({
-      // CONFIGURACIÓN DEL COPY PLUGIN
       patterns: [
         {
-          from: path.resolve(__dirname, "src", "assets/images"), // CARPETA A MOVER AL DIST
-          to: "assets/images", // RUTA FINAL DEL DIST
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
         },
       ],
     }),
